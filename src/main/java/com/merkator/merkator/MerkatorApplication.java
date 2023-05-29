8a9de787068e330ebcf138b9aa167712756032a8
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2023-05-29 13:22:20 +0100

    Initial Commit of Spring Test Project with MongoDB connection.

---
+++ src/main/java/com/merkator/merkator/MerkatorApplication.java
@@ -0,0 +1,13 @@
+package com.merkator.merkator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerkatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merkator/merkator/MerkatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merkator.merkator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerkatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
